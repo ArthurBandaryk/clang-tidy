13e8fb14531674c68119f225bce8111e5dc804f6
Artur <arthur_bandaryk@mail.ru>
2021-12-03 19:00:13 +0300

    Create main.cc

---
+++ test/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+// Just print 'Hello!'.
+int main(int argc, char** argv) {
+  cout << "Hello!" << endl;
+  return cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
